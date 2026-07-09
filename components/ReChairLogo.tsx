a654e66f10782f43361e327a99b49f4761c524dc
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-09 13:55:04 +0900

    Add files via upload

--- components/ReChairLogo.tsx
+++ components/ReChairLogo.tsx
@@ -1,20 +1,20 @@
 export default function ReChairLogo() {
   return (
-    <span className="rc-logo rc-logo-leftmock" aria-label="ReChair">
+    <span className="rc-logo rc-logo-refgear" aria-label="ReChair">
       <span className="rc-logo-words">
         <span className="rc-logo-re">Re</span>
         <span className="rc-logo-chair">Chair</span>
       </span>
 
-      <span className="rc-logo-gears rc-true-gears" aria-hidden="true">
-        <svg className="rc-gear-svg rc-blue-gear" viewBox="0 0 100 100">
-          <path d="M50 4 L58 16 L72 12 L76 27 L91 31 L84 44 L96 54 L82 62 L84 78 L68 78 L58 92 L48 80 L32 88 L28 72 L12 68 L20 54 L8 42 L24 36 L22 20 L38 22 Z" />
-          <circle cx="50" cy="50" r="23" />
+      <span className="rc-logo-gears rc-ref-gears" aria-hidden="true">
+        <svg className="rc-ref-gear rc-ref-gear-big" viewBox="0 0 100 100">
+          <path d="M50 4 C54 4 58 5 62 6 L64 17 C67 18 70 20 73 22 L84 17 C87 20 90 23 92 27 L86 37 C88 40 89 43 90 47 L100 52 C100 56 99 60 98 64 L87 66 C86 69 84 72 82 75 L87 86 C84 89 81 92 77 94 L67 88 C64 90 61 91 57 92 L52 102 C48 102 44 101 40 100 L38 89 C35 88 32 86 29 84 L18 89 C15 86 12 83 10 79 L16 69 C14 66 13 63 12 59 L2 54 C2 50 3 46 4 42 L15 40 C16 37 18 34 20 31 L15 20 C18 17 21 14 25 12 L35 18 C38 16 41 15 45 14 Z" />
+          <circle cx="51" cy="53" r="25" />
         </svg>
 
-        <svg className="rc-gear-svg rc-navy-gear" viewBox="0 0 100 100">
-          <path d="M50 4 L58 16 L72 12 L76 27 L91 31 L84 44 L96 54 L82 62 L84 78 L68 78 L58 92 L48 80 L32 88 L28 72 L12 68 L20 54 L8 42 L24 36 L22 20 L38 22 Z" />
-          <circle cx="50" cy="50" r="23" />
+        <svg className="rc-ref-gear rc-ref-gear-small" viewBox="0 0 100 100">
+          <path d="M50 4 C54 4 58 5 62 6 L64 17 C67 18 70 20 73 22 L84 17 C87 20 90 23 92 27 L86 37 C88 40 89 43 90 47 L100 52 C100 56 99 60 98 64 L87 66 C86 69 84 72 82 75 L87 86 C84 89 81 92 77 94 L67 88 C64 90 61 91 57 92 L52 102 C48 102 44 101 40 100 L38 89 C35 88 32 86 29 84 L18 89 C15 86 12 83 10 79 L16 69 C14 66 13 63 12 59 L2 54 C2 50 3 46 4 42 L15 40 C16 37 18 34 20 31 L15 20 C18 17 21 14 25 12 L35 18 C38 16 41 15 45 14 Z" />
+          <circle cx="51" cy="53" r="25" />
         </svg>
       </span>
     </span>
