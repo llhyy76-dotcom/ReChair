1bc7d94ffff68afdb99ab539b6311b66d06c8bc4
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-13 17:47:15 +0900

    Add files via upload

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,10 @@
 import AdminProducts from '@/components/AdminProducts';
 import './admin-products.css';
 
-export default function AdminProductsPage(){
-  return <main className="rc-admin-product-page"><AdminProducts/></main>;
+export default function AdminProductsPage() {
+  return (
+    <main className="rc-admin-product-page">
+      <AdminProducts />
+    </main>
+  );
 }
