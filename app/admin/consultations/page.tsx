c2e7e07619c36495c940c3459ae2fcf6974a19e8
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-14 17:53:27 +0900

    Add files via upload

---
+++ app/admin/consultations/page.tsx
@@ -0,0 +1,3 @@
+import AdminConsultationsCRM from '@/components/AdminConsultationsCRM';
+import './consultations.css';
+export default function Page(){return <main className="crm-page"><AdminConsultationsCRM/></main>}
