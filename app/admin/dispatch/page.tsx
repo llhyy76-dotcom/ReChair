fe01a6f2a35430e0a46aea1f60ab2897dedef049
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-15 14:55:26 +0900

    Add files via upload

---
+++ app/admin/dispatch/page.tsx
@@ -0,0 +1,3 @@
+import AdminDispatchBoard from '@/components/AdminDispatchBoard';
+import './dispatch.css';
+export default function Page(){return <main className="dispatch-page"><AdminDispatchBoard/></main>}
