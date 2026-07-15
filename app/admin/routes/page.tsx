4961d4c8c3e3d0933f63128af1d495cb7fd863c0
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-15 15:27:00 +0900

    Add files via upload

---
+++ app/admin/routes/page.tsx
@@ -0,0 +1,6 @@
+import AdminRoutePlanner from '@/components/AdminRoutePlanner';
+import './routes.css';
+
+export default function Page(){
+  return <main className="route-page"><AdminRoutePlanner/></main>;
+}
