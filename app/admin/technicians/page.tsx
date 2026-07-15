85054cd687d1830e6fa6af9ca40e6b70f01f270c
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-15 14:07:52 +0900

    Add files via upload

---
+++ app/admin/technicians/page.tsx
@@ -0,0 +1,3 @@
+import AdminTechnicianManager from '@/components/AdminTechnicianManager';
+import './technicians.css';
+export default function Page(){return <main className="tm-page"><AdminTechnicianManager/></main>}
