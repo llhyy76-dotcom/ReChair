f4764f8592406fe358b16d6d68ecafbdf8723938
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-15 11:27:16 +0900

    Add files via upload

---
+++ app/admin/schedule/page.tsx
@@ -0,0 +1,3 @@
+import AdminScheduleBoard from '@/components/AdminScheduleBoard';
+import './schedule.css';
+export default function Page(){return <main className="schedule-page"><AdminScheduleBoard/></main>}
