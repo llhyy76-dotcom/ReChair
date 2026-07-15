9e3e982f66d2f5807589526a4b2a7777d1a98b69
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-15 18:09:59 +0900

    Add files via upload

---
+++ app/technician/login/page.tsx
@@ -0,0 +1,6 @@
+import TechnicianLoginForm from '@/components/TechnicianLoginForm';
+import './login.css';
+
+export default function Page(){
+  return <main className="tech-login-page"><TechnicianLoginForm/></main>;
+}
