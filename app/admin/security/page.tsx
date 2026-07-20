15ae3a639b889e8738d70d8c3e76c4016bb3f830
llhyy76-dotcom <llhyy76@gmail.com>
2026-07-20 14:19:19 +0900

    Add files via upload

---
+++ app/admin/security/page.tsx
@@ -0,0 +1,3 @@
+import AdminSecurityAudit from '@/components/AdminSecurityAudit';
+import './security.css';
+export default function Page(){return <main className="security-page"><AdminSecurityAudit/></main>}
